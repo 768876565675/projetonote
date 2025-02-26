dbabdaa6295926b2e302ca89d5c1d2c95ce70200
matheus santiago <matheusmacedo90000@gmail.com>
2025-02-26 09:28:18 -0300

    jjj

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const name = "matheus";
+  const a = 1;
+  const b = 2;
+
+  function sum (){
+    return a + b;
+  }
+
+  const url = "https://www.pixartprinting.it/blog/wp-content/uploads/2021/06/1_Mona_Lisa_300ppi.jpg";
+
+  return(
+    <div className='App'>
+     <p>Testando o JSX</p>
+     <p>Salve {name} </p>
+     <p> Soma é {sum(1, 2)}</p>
+     <img src='{url}' alt= 'informação da Imagem'></img>
+
     </div>
-  );
+  )
 }
+  
 
 export default App;
